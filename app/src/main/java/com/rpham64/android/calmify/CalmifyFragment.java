31b6bda4c40a299d2a07d267df7d522c477a27ee
rpham64 <rpham64@gmail.com>
2016-05-25 06:04:28 -0700

    Created fullscreen xml file

--- app/src/main/java/com/rpham64/android/calmify/CalmifyFragment.java
+++ app/src/main/java/com/rpham64/android/calmify/CalmifyFragment.java
@@ -3,7 +3,6 @@ package com.rpham64.android.calmify;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,9 +25,7 @@ public class CalmifyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_calmify, container, false);
-
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.calmify_recycler_view);
+        View view = inflater.inflate(R.layout.fragment_full_screen_media_player, container, false);
 
         return view;
     }
